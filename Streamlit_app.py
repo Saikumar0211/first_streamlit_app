50c5b3b4cdf36a3edc49cd2f14aab1a10f482b55
Saikumar venigalla <112375667+Saikumar0211@users.noreply.github.com>
2022-08-29 12:46:59 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,9 +1,11 @@
 
 import streamlit
 streamlit.title('My Parents New Healthy diner')
-streamlit.header('Breakfats Menu')
+streamlit.header('Breakfast favorites')
 streamlit.text('omega 3 & Blueberry')
 streamlit.text('kale & Spinach')
 streamlit.text('half boiled egg')
+
+streamlit.header('bilid you own fruit smoothie')
 
 
