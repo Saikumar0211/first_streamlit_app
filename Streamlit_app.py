abad495320659a237736b4037a8140deca7b7b62
Saikumar venigalla <112375667+Saikumar0211@users.noreply.github.com>
2022-08-29 12:42:19 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,9 @@
 
 import streamlit
 streamlit.title('My Parents New Healthy diner')
+streamlit.header('Breakfats Menu')
+streamlit.text('omega 3 & Blueberry')
+streamlit.text('kale & spinach')
+streamlit.text('half boiled egg')
+
+
